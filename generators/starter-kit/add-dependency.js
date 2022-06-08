ccf8d37a1448babb6e8f3be01e6162ab070b554d
Jay Callicott <jay.callicott@mediacurrent.com>
2022-06-08 12:42:37 -0500

    Updating dependencies

--- generators/starter-kit/add-dependency.js
+++ generators/starter-kit/add-dependency.js
@@ -5,23 +5,90 @@ module.exports = function addDependency (component, themeNameMachine) {
   // If this is the carousel component, it needs SlickJS added
   // as a dependency.
   if (component === 'carousel') {
-    // libraries = {
-    //   [component]: {
-    //     css: {
-    //       component: {
-    //         [`dist/css/${component}.css`]: {}
-    //       }
-    //     },
-    //     js: {
-    //       [`dist/js/${component}.js`]: {}
-    //     },
-    //     dependencies: [
-    //       'core/drupal',
-    //       'core/jquery',
-    //       `${themeNameMachine}/slick-carousel`
-    //     ]
-    //   }
-    // };
+    libraries = {
+      [component]: {
+        css: {
+          component: {
+            [`dist/css/${component}.css`]: {}
+          }
+        },
+        js: {
+          [`dist/js/${component}.es6.js`]: {}
+        },
+        dependencies: [
+          `${themeNameMachine}/slick`
+        ]
+      }
+    };
+  }
+  // Card List depends on Card.
+  else if (component === 'card-list') {
+    libraries = {
+      [component]: {
+        css: {
+          component: {
+            [`dist/css/${component}.css`]: {}
+          }
+        },
+        dependencies: [
+          `${themeNameMachine}/card`,
+        ]
+      }
+    };
+  }
+  // Gallery Lightbox depends on Colorbox.
+  if (component === 'gallery-lightbox') {
+    libraries = {
+      [component]: {
+        css: {
+          component: {
+            [`dist/css/${component}.css`]: {}
+          }
+        },
+        js: {
+          [`dist/js/${component}.js`]: {}
+        },
+        dependencies: [
+          `${themeNameMachine}/colorbox`
+        ]
+      }
+    };
+  }
+  // Main-menu has several JS dependencies.
+  if (component === 'main-menu') {
+    libraries = {
+      [component]: {
+        css: {
+          component: {
+            [`dist/css/${component}.css`]: {}
+          }
+        },
+        js: {
+          [`dist/js/${component}.js`]: {}
+        },
+        dependencies: [
+          'core/jquery',
+          'core/jquery.once',
+          'core/drupal',
+          'core/drupalSettings'
+        ]
+      }
+    };
+  }
+  // Site-header depends on L-Flex.
+  if (component === 'site-header') {
+    libraries = {
+      [component]: {
+        css: {
+          component: {
+            [`dist/css/${component}.css`]: {}
+          }
+        },
+        dependencies: [
+          `${themeNameMachine}/l-flex`
+        ]
+      }
+    };
   }
 
   return Object.keys(libraries).length && libraries;
--- generators/starter-kit/add-third-party.js
+++ generators/starter-kit/add-third-party.js
@@ -2,30 +2,62 @@
 module.exports = function addThirdParty(libraries) {
 
   // See if Slick is one of the libraries already.
-  const containsSlick = libraries.find(lib => lib['slick-carousel']);
+  const containsSlick = libraries.find(lib => lib['slick']);
   // See if the libraries include the carousel library because
   // if so, we need to add the SlickJS dependency.
   const containsCarousel = libraries.find(lib => lib['carousel']);
+  const containsGalleryCarousel = libraries.find(lib => lib['gallery-carousel']);
+
+  // See if Colorbox is one of the libraries already.
+  const containsColorbox = libraries.find(lib => lib['colorbox']);
+  const containsGalleryLightbox = libraries.find(lib => lib['gallery-lightbox']);
 
   // If slick hasn't been added yet and it's needed, add it.
-  if (!containsSlick && containsCarousel) {
-    // const slick = {
-    //   ['slick-carousel']: {
-    //     css: {
-    //       component: {
-    //         ['/libraries/slick-carousel/slick/slick.css']: {}
-    //       }
-    //     },
-    //     js: {
-    //       ['/libraries/slick-carousel/slick/slick.min.js']: { minified: true }
-    //     },
-    //     dependencies: [
-    //       'core/jquery'
-    //     ]
-    //   }
-    // };
-    // // Add slick to the libraries object.
-    // libraries.push(slick);
+  if (!containsSlick && (containsCarousel || containsGalleryCarousel)) {
+    const slick = {
+      ['slick']: {
+        css: {
+          component: {
+            ['dist/css/slick.css']: {},
+            ['dist/css/slick.css-theme']: {}
+          }
+        },
+        js: {
+          ['dist/js/slick.min.js']: { minified: true }
+        },
+        dependencies: [
+          'core/drupal',
+          'core/drupalSettings',
+          'core/jquery.once',
+          'core/jquery'
+        ]
+      }
+    };
+    // Add slick to the libraries object.
+    libraries.push(slick);
+  }
+  // If colorbox hasn't been added yet and it's needed, add it.
+  else if(!containsColorbox && containsGalleryLightbox) {
+    const colorbox = {
+      ['slick']: {
+        css: {
+          component: {
+            ['dist/css/colorbox.css']: {}
+          }
+        },
+        js: {
+          ['dist/js/jquery.colorbox.js']: { minified: true }
+        },
+        dependencies: [
+          'core/drupal',
+          'core/drupalSettings',
+          'core/jquery.once',
+          'core/jquery'
+        ]
+      }
+    };
+    // Add slick to the libraries object.
+    libraries.push(colorbox);
   }
 
   return libraries;
